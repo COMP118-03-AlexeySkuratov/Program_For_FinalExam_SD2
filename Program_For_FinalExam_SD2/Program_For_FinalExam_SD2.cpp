1f421d1ceb058f2dcf321725991ab6b5a4be2c5f
unki113d <unkilled135@gmail.com>
2024-05-27 17:07:18 +0300

    Program setup
    This commit won't be clean s I've lost a big part of my code while restoring acces to building a project.

--- Program_For_FinalExam_SD2/Program_For_FinalExam_SD2.cpp
+++ Program_For_FinalExam_SD2/Program_For_FinalExam_SD2.cpp
@@ -1,20 +1,68 @@
-// Program_For_FinalExam_SD2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/**
+*
+*
+*
+*
+*
+*/
 
 #include <iostream>
 
+using namespace std;
+
+void FillMatrix(const int, const int);
+void ShowMatrix(const int, const int);
+void SumNegative(const int, const int);
+
+const int SIZE_ROW = 5;
+const int SIZE_COL = 3;
+double matrix[SIZE_COL][SIZE_ROW] = {0};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	//Menu implementation
+	int choice = 0;
+
+	cout << "---- M E N U ----" << endl;
+	cout << "1. Enter Data in matrix (range -3.5 to 1.5)" << endl;
+	cout << "2. Show Data in matrix" << endl;
+	cout << "3. Sum of all negatives numbers" << endl;
+	cout << "4. Exit" << endl;
+	cout << "Please, enter your choice: ___" << endl;
+
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		FillMatrix(SIZE_COL, SIZE_ROW);
+		break;
+	case 2:
+		ShowMatrix(SIZE_COL, SIZE_ROW);
+		break;
+	case 3:
+		SumNegative(SIZE_COL, SIZE_ROW);
+		break;
+	case 4:
+		cout << "Have a nice day!" << endl;
+		break;
+	default:
+		break;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void FillMatrix(double[], const int, const int)
+{
+
+
+}
+//void ShowMatrix(double[], const int, const int)
+//{
+//
+//}
+//void SumNegative(double[], const int, const int)
+//{
+//
+//}
