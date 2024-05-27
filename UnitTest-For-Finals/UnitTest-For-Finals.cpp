d8a3da25147311f56356218da1e68938440b8a95
unki113d <unkilled135@gmail.com>
2024-05-27 18:07:56 +0300

    Added a new file: UnitTest-For-Finals.cpp
    Working on implementing unit test features

---
+++ UnitTest-For-Finals/UnitTest-For-Finals.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program_For_FinalExam_SD2/Program_For_FinalExam_SD2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestForFinals
+{
+	TEST_CLASS(UnitTestForFinals)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//Arrange
+
+			//Act
+
+			//Assert
+
+		}
+	};
+}
